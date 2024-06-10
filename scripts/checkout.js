2e07f34a112314ed8a61eef1c09e133f4d25de3a
Amin <incamin3@gmail.com>
2024-06-10 03:19:27 +0200

    sepatrate functions

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummary +=`
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-container-${matchingProduct.id} ">
 <div class="delivery-date">
   Delivery date: Tuesday, June 21
 </div>
@@ -31,7 +31,7 @@ cart.forEach((cartItem) => {
       ${matchingProduct.name}
     </div>
     <div class="product-price">
-      $${formatCurrency(matchingProduct)}
+      $${formatCurrency(matchingProduct.priceCents)}
     </div>
     <div class="product-quantity">
       <span>
@@ -40,7 +40,8 @@ cart.forEach((cartItem) => {
       <span class="update-quantity-link link-primary">
         Update
       </span>
-      <span class="delete-quantity-link link-primary">
+      <span class="delete-quantity-link link-primary js-delete-link"
+      data-product-id = "${matchingProduct.id} ">
         Delete
       </span>
     </div>
@@ -100,3 +101,12 @@ cart.forEach((cartItem) => {
     .innerHTML = cartSummary;
 
 });
+
+document.querySelectorAll ('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+      console.log(cart);
+    });
+  });
